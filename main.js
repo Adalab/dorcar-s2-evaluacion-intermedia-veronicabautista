d95bc25e62a8065f23307662967741f75e759721
Veronica Bautista <veronicabautista87@gmail.com>
2018-06-29 09:31:59 +0200

    subir

---
+++ main.js
@@ -0,0 +1,19 @@
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+
+ console.log('> ' + getRandomNumber(100));
+
+var getnumber = getRandmonNumber;
+var prueba = document.querySelector('.button');
+prueba.addEventListener('click', showNumber);
+
+
+function showNumber(){
+    if (){
+    } 
+  
+
+
+var numberInput = document.querySelector('.writenumber');
+var number= parseInt(numberInput);
